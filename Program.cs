c01b9a1ae4da4c76981d1939107c1dbbfb5fda49
Yuliya <darkwader@mail.ru>
2023-03-04 15:28:40 +0300

    FillMatrix

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿void FillMatrix(int [,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+        mat[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// int [,] matrix = new int [3, 4];
+// int [,] matrix = Input
+// PrintMatrix(matrix);
+// Console.WriteLine();
